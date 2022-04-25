701c3757a34fa4975f4ea54b723a7f724e77fed7
lagron1000 <37865939+lagron1000@users.noreply.github.com>
2022-04-25 20:31:22 +0300

    Add files via upload

---
+++ 1/test/do.c
@@ -0,0 +1,10 @@
+int max(int num1, int num2);
+ 
+int main () {
+
+   /* local variable definition */
+   int a = 100;
+   int cocka = 200;
+
+   return 0;
+}
---
+++ 1/test/do2.c
@@ -0,0 +1,10 @@
+int max(int num1, int num2);
+ 
+int main () {
+
+   /* local variable definition */
+   int a = 100;
+   int c = 200;
+
+   return 0;
+}
